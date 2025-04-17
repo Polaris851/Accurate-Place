eccf8a8115c2feed5cea96e60e144e31e3db592b
Polaris851 <leticiapolari8@gmail.com>
2025-04-16 21:12:08 -0300

    web: added home

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,14 +3,19 @@ import { HostsPage } from "./pages/hosts/page";
 import { HostPage } from "./pages/host/page";
 import { Login } from "./pages/login/page";
 import { ClientsPage } from "./pages/clients/page";
+import { Home } from "./pages/home/page";
 
 export function RouteManager() {
     return (
         <Routes>
-            {/* <Route path="/" element={<Login/>} /> */}
-            <Route path="/" element={<HostsPage/>} />
+            <Route path="/" element={<Login/>} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/host" element={<HostsPage/>} />
             <Route path="/host/:hostId" element={<HostPage/>} />
             <Route path="/client" element={<ClientsPage />} />
+            {/* <Route path="/client/:clientId" element={<ClientsPage />} /> */}
+            {/* <Route path="/reservation" element={<ClientsPage />} /> */}
+            {/* <Route path="/reservation/:clientId" element={<ClientsPage />} /> */}
         </Routes>
     )
 }
